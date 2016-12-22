de3b6c971651ac84e9e7a377aafcfba69002ab1b
Mateusz Stadnik <matgla@live.com>
2016-12-22 18:22:44 +0000

    clean project from unused msos files

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,24 +1,8 @@
 #include "kernel.hpp"
-#include "lcd.h"
-#include "sdram.h"
 #include "usart.hpp"
-#include "usb.h"
 
 void hardwareInitialize() {
-    // sdram initalization
-    GPIO_FMC_init();
     USART_GPIO_init();
-
-    FMC_init();
-
-    // video initalzation
-    SettingsInitialize();
-
-    LCD_init();
-
-    LCD_layerInit();
-    // usb initalization
-    USBInit();
 
     USART_NVIC_init();
     USART_init();
