27090793398af3cb2e62650b7a2f98d080ede30e
Mateusz Stadnik <matgla@live.com>
2018-07-24 22:29:12 +0200

    added X86 simulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
-#include "hal/gpio/digitalOut.hpp"
+#include "hal/gpio.hpp"
+#include "hal/time/sleep.hpp"
+
+#include <chrono>
+
 
 int main()
 {
-    hal::gpio::DigitalOut<hal::gpio::Port::A> led(1);
-    led.high();
-    led.low();
+    constexpr PA0 pin;
+    constexpr PA1 pin2;
+    pin.init(hal::gpio::GpioSpeed::Low,
+             hal::gpio::GpioMode::OutputPushPull);
+
+    pin.setLow();
+    pin.setHigh();
 
     while (true)
     {
+        hal::time::sleep(std::chrono::seconds(1));
+        pin2.setHigh();
+        hal::time::sleep(std::chrono::seconds(1));
+        pin2.setLow();
     }
 
     return 0;
