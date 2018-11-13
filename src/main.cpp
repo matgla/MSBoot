464b57be4ba7256d05d37d8e350552a524fb57ed
Mateusz Stadnik <matgla@live.com>
2018-11-13 19:57:21 +0100

    building via new script

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,35 @@
 #include "eul/logger.hpp"
 #include "hal/gpio.hpp"
 #include "hal/time/sleep.hpp"
-#include "hal/usart.hpp"
+// #include "hal/usart.hpp"
 
 #include <chrono>
 
+extern "C"
+{
+    void _init()
+    {
+    }
+}
 
 int main()
 {
-    constexpr hal::gpio::PA0 pin;
-    constexpr hal::gpio::PA1 pin2;
-    pin.init(hal::gpio::GpioSpeed::Low,
-             hal::gpio::GpioMode::OutputPushPull);
+    // constexpr hal::gpio::PA0 pin;
+    // constexpr hal::gpio::PA1 pin2;
+    // pin.init(hal::gpio::GpioSpeed::Low,
+    //          hal::gpio::GpioMode::OutputPushPull);
 
-    pin.setLow();
-    pin.setHigh();
-    hal::usart::USART1::init();
-    eul::Logger<hal::usart::USART1> logger("main");
+    // pin.setLow();
+    // pin.setHigh();
+    // hal::usart::USART1::init();
+    // eul::Logger<hal::usart::USART1> logger("main");
     while (true)
     {
-        logger.info() << "working...";
-        hal::time::sleep(std::chrono::seconds(1));
-        pin2.setHigh();
-        hal::time::sleep(std::chrono::seconds(1));
-        pin2.setLow();
+        // logger.info() << "working...";
+        // hal::time::sleep(std::chrono::seconds(1));
+        // pin2.setHigh();
+        // hal::time::sleep(std::chrono::seconds(1));
+        // pin2.setLow();
     }
 
     return 0;
