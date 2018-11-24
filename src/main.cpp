9b0a48c8b3ee43e6ac5ebe5e751adfbc280dea3c
Mateusz Stadnik <matgla@live.com>
2018-11-24 12:32:15 +0100

    aligned implementation to fixes in hal

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,26 @@
+#include <chrono>
+#include <cstdint>
+
+#include <gsl/span>
+
 #include "eul/logger.hpp"
 #include "hal/gpio.hpp"
 #include "hal/time/sleep.hpp"
 #include "hal/time/time.hpp"
 #include "hal/usart.hpp"
 
-#include <chrono>
-
-extern "C"
-{
-    void _init()
-    {
-    }
-}
-
 int main()
 {
     constexpr hal::gpio::PB12 pin;
-
+    constexpr hal::interfaces::Usart1 usart;
     hal::time::Time::init();
     // constexpr hal::gpio::PA1 pin2;
     pin.init(hal::gpio::Output::OutputPushPull, hal::gpio::Speed::Low);
     //pin.setHigh();
-    hal::interfaces::Usart1::init(9600);
-    hal::interfaces::Usart1::write("aaa");
+    usart.init(9600);
+    usart.write("aaa");
     // eul::Logger<hal::usart::Usart1> logger("main");
+    usart.onData([&usart](const uint8_t data) { usart.write(data); });
     while (true)
     {
 
@@ -32,7 +29,7 @@ int main()
         pin.setHigh();
         hal::time::sleep(std::chrono::seconds(1));
         pin.setLow();
-        hal::interfaces::Usart1::write("bv");
+        // usart.write("bv");
     }
 
     return 0;
