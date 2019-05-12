04b3e9d1e4a8af7bcae9c4e57b61774700570791
Mateusz Stadnik <matgla@live.com>
2019-05-12 18:52:06 +0200

    added modules management

---
+++ cmake/GitModules.cmake
@@ -0,0 +1,43 @@
+function (execute_command command_to_execute)
+    message (STATUS "Executing: ${command_to_execute}")
+    find_package(Git QUIET)
+    if (NOT GIT_FOUND)
+        message (FATAL_ERROR "Can't find git")
+    endif ()
+
+    string(REPLACE " " ";" command_to_execute "${command_to_execute}")
+
+    set (command_ git submodule foreach git checkout master)
+    execute_process(
+        COMMAND
+            ${command_to_execute}
+        WORKING_DIRECTORY
+            ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE output
+        ERROR_VARIABLE error
+        RESULT_VARIABLE result
+    )
+
+    if (NOT result EQUAL "0")
+        message ("Failure: ${OUTPUT_VARIABLE}")
+        message (FATAL_ERROR "Failure: ${ERROR_VARIABLE}")
+    endif ()
+endfunction()
+
+function (fetch_module module_name)
+    message (STATUS "Update module: ${module_name}")
+    find_package(Git QUIET)
+    if (NOT GIT_FOUND)
+        message (FATAL_ERROR "Can't find git")
+    endif ()
+
+    if (NOT TARGET ${module_name})
+        execute_process(
+            COMMAND
+                git submodule update --init -- lib/${module_name}
+            WORKING_DIRECTORY
+                ${PROJECT_SOURCE_DIR}
+        )
+        add_subdirectory(${module_name})
+    endif ()
+endfunction()
